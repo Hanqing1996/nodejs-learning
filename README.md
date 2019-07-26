b2e82ec6e28f897b92de1062fc161a6e3d705f46
Hanqing1996 <zhq199622@163.com>
2019-07-26 20:48:31 +0800

    add http tests

--- README.md
+++ README.md
@@ -20,6 +20,45 @@ Schema://host:port/path?query#hash
 ```
 address://putong.shanghai.china/jinkeroad/1111'
 ```
+* but
+> 对于‘localhost:8000/user?quan=3&people=china‘
+```
+const url=req.url; // url='/user?quan=3&people=china'
+```
 * 为什么url通常看不见port
 > 存在默认端口(80,443)
+* HTTP方法分类
+1. GET 
+> 获取某种资源
+2. POST
+> 新建
+3. PATCH
+> 修改
+4. OPTIONS
+> 列举可进行的操作
 
+* HTTP头部组成：
+<br>
+1. 第一行:方法、路径、协议版本，使用空格分割
+```
+GET /user HTTP/1.1
+```
+2. 第二行开始:内容
+* HTTP头中常见键值对：
+1. Host 指明请求的地址
+2. Server 服务器信息
+3. Content-Type 请求体的格式,如 xxx/www-url-encoded-form-data application/json
+4. Accept: 接受的返回格式
+5. Accept-Encoding：接受的请求编码
+6. Accept-Language：接受的语言
+7. Pragma：兼容http1.0的缓存
+8. Cache-Control:缓存策略
+9. Via:走过的服务器链路信息
+
+* Response头第一行： 
+> 协议 状态码 状态描述 如 HTTP/1.1 200 OK
+* 返回码：
+2. 2xx 请求成功 200成功 201创建 202接受 203代理 206部分处理
+3. 3XX 重定向 302找到，重定向 304未修改
+4. 4xx 客户端错误 400请求内容错误 401无权限 403禁止访问 404你懂的
+5. 5xx 服务器错误 500服务器炸了 502网关炸了 503炸几分钟 504超时
--- simple_http_server.js
+++ simple_http_server.js
@@ -1,25 +1,136 @@
-const http=require('http');
+const http=require('http')
 const server=http.createServer();
 server.listen(8000);
 
 // 根据不同url返回不同响应结果
-// let count=0
-// server.on('request',(req,res)=>{
+/*
+// 输入:localhost:8000/hello 
+server.on('request',(req,res)=>{
 
-//     const url=req.url; // 用户向浏览器发送一个请求地址
+    const url=req.url; // /hello
+    console.log(url)
 
-//     let responseStr
+    let responseStr
 
-//     if(url==='/hello'){ // ===表示完全匹配
-//         responseStr='hi there'
-//     }
-//     else if(url==='/bye'){
-//         responseStr='see yoy tomorrow'
-//     }
-//     else{
-//         responseStr='I know nothing'
-//     }
+    if(url==='/hello'){ // ===表示完全匹配
+        responseStr='hi there'
+    }
+    else if(url==='/bye'){
+        responseStr='see yoy tomorrow'
+    }
+    else{
+        responseStr='I know nothing'
+    }
     
-//     res.statusCode=200;
-//     res.end(responseStr);
-// });
+    res.statusCode=200;
+    res.end(responseStr);
+});
+*/
+
+
+
+// 解析query
+/*
+// 输入:localhost:8000?i_need_money=true&how_much=1000
+const qs=require('querystring');
+server.on('request',(req,res)=>{
+    
+    const url=req.url;// /?i_need_money=true&how_much=1000
+    const querystr=url.substr(url.indexOf('?')+1,url.length); // i_need_money=true&how_much=1000
+    const query=qs.parse(querystr); // query为一个对象
+
+    // 提取query参数
+    if(query.i_need_money==='true'&&query.how_much==='1000')
+    {
+        res.statusCode=200;
+        res.end('go away');
+    }
+});
+*/
+
+// 解析path,query.根据不同method返回不同内容
+
+// 输入:localhost:8000/user?quan=3&people=china
+/*
+let temp=4
+server.on('request',(req,res)=>{
+
+    users=[1,2,3];
+
+    const url=req.url; // /user?quan=3&people=china
+    const path=url.substr(0,url.indexOf('?')) // /user
+    const query=url.substr(url.indexOf('?')+1,url.length) // quan=3&people=china
+
+    switch(path){
+        case '/user':
+
+            // 注意req.method和req.url没有关系
+            switch(req.method){
+                case 'GET':
+                    res.statusCode=200;
+                    res.end(JSON.stringify(users)) // 获取users
+                    break;
+                case 'POST':
+                    users.push(temp++);
+                    res.statusCode=200;
+                    res.end(JSON.stringify(users)) // 增加user
+                    break;    
+            }
+            break;
+
+        default:
+            res.statusCode=404
+            res.end('')
+            break;    
+    } 
+});
+*/
+
+// 解析HTTP请求体
+
+// 输入:localhost:8000/user?quan=3&people=china
+server.on('request',(req,res)=>{
+
+    const users=[];
+    const url=req.url; // /user?quan=3&people=china
+    const path=url.substr(0,url.indexOf('?')) // /user
+    const query=url.substr(url.indexOf('?')+1,url.length) // quan=3&people=china
+
+    switch(path){
+        case '/user':
+
+            switch(req.method){
+                case 'GET':
+                    res.statusCode=200;
+                    res.end(JSON.stringify(users))
+                    break;
+                case 'POST':
+
+                    const contentType=req.headers['content-type']
+                  
+                    // 指定请求体内容格式
+                    if(contentType!=='application/json'){
+                        res.statusCode=400;
+                        res.end('error');
+                    }
+
+                    let requestBodtStr=''
+                    req.on('data',function(data){
+                        requestBodtStr+=data.toString();
+                    });
+                    req.on('end',function(){
+                        const user=JSON.parse(requestBodtStr);
+                        users.push(user);
+                        res.statusCode=200;
+                        res.end(JSON.stringify(user));
+                    });
+                    break;    
+            }
+            break;
+        
+        default:
+            res.statusCode=404
+            res.end('')
+            break;    
+    } 
+});
