2b2fac0cb4c3025ac80de5b22926c0503fd66380
Hanqing1996 <zhq199622@163.com>
2019-07-29 14:29:42 +0800

    analysis request body

--- express_demo/index.js
+++ express_demo/index.js
@@ -28,7 +28,7 @@ server.listen('8888');
 
 
 // 中间件应用:鉴权
-
+/*
 // 输入:localhost:8888?username=liming&&age=14
 function auth(req,res,next){
     if(req.query.username==='liming'){
@@ -48,3 +48,74 @@ app.use((req,res,next)=>{
 const server=http.createServer(app);
 
 server.listen('8888');
+*/
+
+
+
+// 错误中间件
+/*
+app.use((req,res,next)=>{
+
+    // next被放入参数,直接跳转到错误中间件
+    next('something wrong'); 
+});
+
+// 这个中间件不会执行
+app.use((req,res,next)=>{ 
+    res.end('all right')
+});
+
+// 错误中间件处理错误
+app.use((err,req,res,next)=>{
+    res.end(err);
+});
+
+const server=http.createServer(app);
+
+server.listen('8888');
+*/
+
+
+// 中间件的函数写法
+/*
+function mw1(req,res,next){
+    console.log('mw1');
+    next();
+}
+
+function mw2(req,res,next){
+    console.log('mw2');
+    next();
+}
+
+function mw3(req,res,next){
+    console.log('mw3');
+    res.end()
+}
+
+// 等价于app.use([mw1,mw2],mw3)
+app.use(mw1,mw2,mw3)
+
+const server=http.createServer(app);
+
+server.listen('8888');
+*/
+
+
+// 解析请求体
+// 详见https://xiedaimala.com/tasks/36b40d29-f099-4f18-a945-6a51d3d0ec9c/video_tutorials/8eccf75f-6861-4ae1-a93d-4a91d937e276
+// urlencoded(键值对)和json是两种Content-Type,可在postman中设置
+// 输入:localhost:8888，method选择post
+const bodyParser=require('body-parser');
+app.use(bodyParser.json()); 
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.use((req,res,next)=>{
+    console.log(req.body);
+    res.end('');
+});
+
+const server=http.createServer(app);
+
+server.listen('8888');
+
